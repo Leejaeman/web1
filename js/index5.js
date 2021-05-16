ad5fb588c9fcea2bb746c608fc77c76518e64277
Lee Jaeman <lividious@naver.com>
2021-05-17 07:37:57 +0900

    영어 심리 사이트 업로드

---
+++ js/index5.js
@@ -0,0 +1,25 @@
+function next(){
+    var radio = document.querySelectorAll('input[name=answer]');
+    var selType = null;
+    for(let i = 0; i < radio.length; ++i)
+    {
+        if(radio[i].checked === true)
+        {
+            selType = radio[i].value;
+            break;
+        }
+    }
+
+    if(selType === null)
+    {
+        alert('답을 선택하세요!');
+        return;
+    }
+
+    var index = document.querySelector('input[name=answer]:checked').value;
+    let ans = "2";
+    if(index === ans)
+        location.href="index8.html";
+    else
+        location.href="typec.html";
+}
